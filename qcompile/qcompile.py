9775dd8f11d6ed8cb317ddd26181af167e35dac5
Jay Soni <jbsoni@uwaterloo.ca>
2020-09-11 23:39:56 -0400

    added a gitignore file. Playing around with the Qiskit circuit class to extract the quantum gate information

--- qcompile/qcompile.py
+++ qcompile/qcompile.py
@@ -1,1 +1,26 @@
 # Main file for Quantum Compiler
+from qiskit import *
+
+
+def main():
+    circ = qiskit.QuantumCircuit(3)
+    circ.h(0)
+    circ.cx(0, 2)
+    circ.z(1)
+    circ.draw()
+
+    meta = circ.data
+    print(meta)
+    print(type(meta))
+
+    inst = circ.to_instruction()
+    print(inst)
+    print(inst.decompositions)
+    print(inst.definition)
+    print(inst.params)
+
+    return
+
+
+if __name__ == '__main__':
+    main()
